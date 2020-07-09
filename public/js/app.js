582fdd6346dd5d78ed52e47cf2516b12290f08d8
mustakrakishe <mustakrkish@gmail.com>
2020-07-09 15:53:45 +0300

    start state

--- public/js/app.js
+++ public/js/app.js
@@ -23391,8 +23391,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\documents\book\jewellery\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\documents\book\jewellery\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\documents\book\try\clone\jewellery\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\documents\book\try\clone\jewellery\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
